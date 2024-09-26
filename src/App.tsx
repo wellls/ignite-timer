7ea7ce40e0e946d8f4bfa854228a3959c819c9f9
Wellington Lima <wellington.map@hotmail.com>
2024-09-26 09:36:40 -0300

    chore: start app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
+import { defaultTheme } from "./styles/themes/default"
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </div>  
+    </ThemeProvider>  
   )
 }
 
